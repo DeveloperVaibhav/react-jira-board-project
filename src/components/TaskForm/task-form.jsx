d507c9f214de1c1698a7649e16b51d321fbe9003
Vaibhav Chafle <vaibhav-chafle@Vaibhavs-MacBook-Air.local>
2026-01-21 01:38:00 +0530

    creating jira board react application

---
+++ src/components/TaskForm/task-form.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './task-form.css';
+
+const TaskForm = () => {
+    return (
+        <>
+            <header className='app_header'>
+                <form>
+                    <input type='text' className='task_input' placeholder='Enter Task Name' />
+                    <div className='task_form_bottom'>
+                        <div>
+                            <button className='tag'>DEV</button>
+                            <button className='tag'>QA</button>
+                            <button className='tag'>Product Owner</button>
+                        </div>
+                        <div>
+                            <select className='task_status'>
+                                <option value='Ready for Development'>Ready for Development</option>
+                                <option value='In Progress'>In Progress</option>
+                                <optio value='Ready for Testing'>Ready for Testing</optio>
+                                <option value='Closed'>Closed</option>
+                            </select>
+                            <button type='submit' className='task_submit'>+ Add</button>
+                        </div>
+                    </div>
+                </form>
+            </header>
+        </>
+    )
+}
+
+export default TaskForm
