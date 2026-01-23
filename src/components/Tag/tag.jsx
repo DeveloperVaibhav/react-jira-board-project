276cba37308523057ee246190299a0acff825a8c
DeveloperVaibhav <vaibhavchafle09@gmail.com>
2026-01-24 00:22:51 +0530

    refactoring code

--- src/components/Tag/tag.jsx
+++ src/components/Tag/tag.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
 import './tag.css';
+import { tagStyle, tagKeyMap } from '../../constant/common';
 
 const Tag = (props) => {
   //console.log(props);
   const { tagName, selectedTag, selected } = props;
-  const tagStyle = {
-    DEV: { backgroundColor: '#04caff' },
-    QA: { backgroundColor: '#f17e58' },
-    PO: { backgroundColor: '#62ed42' }
-  }
-  const tagKeyMap = {
-    DEV: 'DEV',
-    QA: 'QA',
-    'Product Owner': 'PO'
-  }
 
   const key = tagKeyMap[tagName];
 
---
+++ src/constant/common.js
@@ -0,0 +1,10 @@
+export const tagStyle = {
+    DEV: { backgroundColor: '#04caff' },
+    QA: { backgroundColor: '#f17e58' },
+    PO: { backgroundColor: '#62ed42' }
+};
+export const tagKeyMap = {
+    DEV: 'DEV',
+    QA: 'QA',
+    'Product Owner': 'PO'
+};
